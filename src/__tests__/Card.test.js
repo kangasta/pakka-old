7f9f5fff90c0dc44f1b70f49784348f22a35f9bb
Toni Kangas <toni.ar.kangas@gmail.com>
2017-12-19 23:34:12 +0200

    Use enzyme.

--- src/__tests__/Card.test.js
+++ src/__tests__/Card.test.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { mount, shallow } from 'enzyme';
 
 import Card from './../Card';
 
 describe('Card', () => {
 	it('renders without crashing', () => {
-		const div = document.createElement('div');
-		ReactDOM.render(<Card />, div);
+		mount(<Card />);
+	});
+	it('has default on click callback', () => {
+		const wrapper = shallow(<Card />);
+		wrapper.find('.card').simulate('click');
+	});
+	it('allows empty, hidden, and visible states', ()=>{
+		const state = ['empty', 'hidden', 'visible'];
+		for (var i = -2; i < 1; i++) {
+			const wrapper = mount(<Card number={i}/>);
+			expect(wrapper.find('.card-' + state[i + 2]).exists()).toBe(true);
+		}
+	});
+	it('has poker and bridge sizes',()=>{
+		const sizes = ['poker', 'bridge', 'cat'];
+		for (var i = 0; i < 3; i++) {
+			const wrapper = mount(<Card size={sizes[i]}/>);
+			expect(wrapper.find('.card-size-' + sizes[i % 2]).exists()).toBe(true);
+		}
+	});
+	it('has different number and face styles',()=>{
+		for (var i = 0; i < 13; i++) {
+			const wrapper = mount(<Card number={i}/>);
+			expect(wrapper.find('.card-style-' + ((i % 13) > 9 ? 'face' : 'number')).exists()).toBe(true);
+		}
 	});
 });
--- src/__tests__/Deck.test.js
+++ src/__tests__/Deck.test.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
 
 import Deck from './../Deck';
 
 describe('Deck', () => {
 	it('renders without crashing', () => {
-		const div = document.createElement('div');
-		ReactDOM.render(<Deck />, div);
+		mount(<Deck />);
 	});
 });
-
-
