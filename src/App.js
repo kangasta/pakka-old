2c52c0fc2951276a7cdbb68b9954a1257513910a
Toni Kangas <toni.ar.kangas@gmail.com>
2017-12-20 21:42:38 +0200

    Show only deck.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import Card from './Card';
 import DeckUtils from './DeckUtils';
-//import Deck from './Deck';
+import Deck from './Deck';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			'card_number': -1,
-			'cards_left': 52
-		};
 
 		this.deck = new DeckUtils();
+		this.has_separate_deck = false;
+
+		this.state = {
+			'card_number': (this.has_separate_deck ? -2 : -1),
+			'cards_left': 52
+		};
 
 		this.resetDeck = this.resetDeck.bind(this);
 		this.updateCard = this.updateCard.bind(this);
@@ -20,11 +22,11 @@ class App extends Component {
 
 	resetDeck() {
 		this.deck.resetDeck();
-		this.setState({'card_number': -1,'cards_left':this.deck.cardsLeft()});
+		this.setState({'card_number': (this.has_separate_deck ? -2 : -1),'cards_left':this.deck.cardsLeft()});
 	}
 
 	updateCard() {
-		if (this.state.card_number === -2) {
+		if ((this.state.card_number === -2 && !this.has_separate_deck) || (!this.deck.cardsLeft() && this.has_separate_deck)) {
 			this.resetDeck();
 			return;
 		}
@@ -42,16 +44,29 @@ class App extends Component {
 		return (
 			<div className='App'>
 				<div className='app-center-container'>
-					<div className='app-card-container app-horizontally-center'>
-						<Card size='poker' number={this.state.card_number} fourcolor={true} onClickCallback={this.updateCard}/>
+					<div className='app-deck-container app-horizontally-center'>
+						<Deck
+							size='poker'
+							fourcolor={false}
+							top_card={this.state.card_number}
+							num_cards={this.state.cards_left}
+							onClickCallback={this.updateCard}
+						/>
 					</div>
 				</div>
 			</div>
 		);
 		/*
-		<br/>
 		<div className='app-deck-container app-horizontally-center'>
-			<Deck size='poker' />
+			<Deck
+				size='poker' fourcolor={true}
+				top_card={this.state.card_number}
+				num_cards={this.state.cards_left}
+				onClickCallback={this.updateCard}
+			/>
+		</div>
+		<div className='app-card-container app-horizontally-center'>
+			<Card size='poker' number={this.state.card_number} fourcolor={true} onClickCallback={this.updateCard}/>
 		</div>
 		*/
 	}
