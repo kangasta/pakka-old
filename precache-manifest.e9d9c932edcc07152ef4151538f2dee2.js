89c091bf45935f9c3e43e752a388158e10da809e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-06 20:28:47 +0000

    Deploy kangasta/pakka to github.com/kangasta/pakka.git:gh-pages

---
+++ precache-manifest.e9d9c932edcc07152ef4151538f2dee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25fbf488fa31370e984bf2c125aac030",
+    "url": "/pakka-old/index.html"
+  },
+  {
+    "revision": "ceb4d192ee48ba4be253",
+    "url": "/pakka-old/static/css/main.3a17bc1f.chunk.css"
+  },
+  {
+    "revision": "ff4535e8c491fdbcaeba",
+    "url": "/pakka-old/static/js/2.0b428717.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pakka-old/static/js/2.0b428717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceb4d192ee48ba4be253",
+    "url": "/pakka-old/static/js/main.4f792844.chunk.js"
+  },
+  {
+    "revision": "ae163790d39eec55da14",
+    "url": "/pakka-old/static/js/runtime-main.025a3d0a.js"
+  }
+]);
